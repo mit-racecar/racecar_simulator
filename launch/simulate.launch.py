19872e8e05ae2ca76d11e0ea249a8ae3eea205da
Mateusz Lichota <mateusz@lichota.net>
2021-10-18 13:18:57 +0100

    refactor simulate.launch for maximum clarity

--- launch/simulate.launch.py
+++ launch/simulate.launch.py
@@ -2,34 +2,30 @@ from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node, LifecycleNode
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import DeclareLaunchArgument, LogInfo, IncludeLaunchDescription, EmitEvent, ExecuteProcess, RegisterEventHandler, include_launch_description
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, EmitEvent, ExecuteProcess, RegisterEventHandler
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 import launch
-import lifecycle_msgs
+import lifecycle_msgs.msg
 from launch_ros.events.lifecycle import ChangeState
 from launch_ros.event_handlers import OnStateTransition
-from launch.event_handlers import OnProcessStart, OnProcessExit
+from launch.event_handlers import OnProcessExit
+from datetime import timedelta
+
+def delayed(e: EmitEvent, by: timedelta) -> ExecuteProcess:
+    return ExecuteProcess(
+        cmd=['sleep', str(by.total_seconds())],
+        on_exit=[
+            e
+        ]
+    )
 
 def generate_launch_description():
-    package = get_package_share_directory('racecar_simulator')
+    package_dir = get_package_share_directory('racecar_simulator')
 
-    default_rviz_config_path = PathJoinSubstitution(
-        [
-            package,
-            'config',
-            'rviz_config.rviz'
-        ])
-    
-    map_launch_arg = DeclareLaunchArgument(
-        name='map',
-        default_value= f'{package}/maps/building_31.yaml')
 
-    rvizconfig_launch_arg = DeclareLaunchArgument(
-        name='rvizconfig', 
-        default_value=default_rviz_config_path,
-        description='Absolute path to rviz config file')
-
-    # RVIZ
+    # ============
+    # ==  RVIZ  ==
+    # ============
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
@@ -45,7 +41,23 @@ def generate_launch_description():
             ],
         ))
 
-    # MAP SERVER
+    default_rviz_config_path = PathJoinSubstitution(
+        [
+            package_dir,
+            'config',
+            'rviz_config.rviz'
+        ])
+
+    rvizconfig_launch_arg = DeclareLaunchArgument(
+        name='rvizconfig', 
+        default_value=default_rviz_config_path,
+        description='Absolute path to rviz config file')
+
+
+
+    # ================ 
+    # == MAP SERVER ==
+    # ================
     map_server_node = LifecycleNode(
         package='nav2_map_server',
         executable='map_server',
@@ -53,51 +65,39 @@ def generate_launch_description():
         output='screen',
         namespace='',
         parameters=[
-            {'yaml_filename': f'{package}/maps/building_31.yaml'}
+            {'yaml_filename': f'{package_dir}/maps/building_31.yaml'}
         ])
 
-    map_server_activate_trans_event = EmitEvent(
+    map_server_emit_activation_event = EmitEvent(
         event = ChangeState(
             lifecycle_node_matcher = launch.events.matches_action(map_server_node),
-            transition_id = lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE,
+            transition_id = lifecycle_msgs.msg.Transition.TRANSITION_ACTIVATE, # type: ignore
         ))
 
-    map_server_configure_trans_event = EmitEvent(
+    map_server_emit_configure_event = EmitEvent(
         event=ChangeState(
             lifecycle_node_matcher = launch.events.matches_action(map_server_node),
-            transition_id = lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE,
+            transition_id = lifecycle_msgs.msg.Transition.TRANSITION_CONFIGURE, # type: ignore
         ))
 
-        
     map_server_inactive_state_handler = RegisterEventHandler(
         OnStateTransition(
             target_lifecycle_node = map_server_node,
             goal_state = 'inactive',
             entities = [
-                LogInfo( msg = "'map_server' reached the 'INACTIVE' state, 'activating'." ),
-                map_server_activate_trans_event,
-            ],
-        ))
-    
-    map_server_active_state_handler = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node = map_server_node,
-            goal_state = 'active',
-            entities = [
-                # Log
-                LogInfo( msg = "'map_server' reached the 'ACTIVE' state" ),
+                map_server_emit_activation_event,
             ],
         ))
 
-    delay_map_server_configure = ExecuteProcess(
-        cmd=['sleep', '1'],
-        on_exit=[
-            map_server_configure_trans_event
-        ])
+    map_launch_arg = DeclareLaunchArgument(
+        name='map',
+        default_value= f'{package_dir}/maps/building_31.yaml')
 
-    include_racecar_model_launch = IncludeLaunchDescription(
+
+
+    include_racecar_model_launch_description = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            f'{package}/launch/racecar_model.launch.py'
+            f'{package_dir}/launch/racecar_model.launch.py'
         ))
 
     simulator_node = Node(
@@ -106,7 +106,7 @@ def generate_launch_description():
         name='racecar_simulator',
         output='screen',
         parameters=[
-            f'{package}/config/params.yaml'
+            f'{package_dir}/config/params.yaml'
         ])
 
     ld = LaunchDescription([
@@ -115,13 +115,12 @@ def generate_launch_description():
     
         map_server_node,        
         map_server_inactive_state_handler,
-        map_server_active_state_handler,
-        delay_map_server_configure,
+        delayed(map_server_emit_configure_event, by=timedelta(seconds=1.5)),
 
         rviz_node,
         shutdown_on_rviz_exit,
 
-        include_racecar_model_launch,
+        include_racecar_model_launch_description,
 
         simulator_node,
     ])
